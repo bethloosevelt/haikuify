82264645a614b51b30e172077d235a0849be49c7
Dale Wesa <wesa.dale@gmail.com>
2016-04-26 18:57:51 -0500

    finds all haikus in a text file and prints them out nicely

--- src/demo/core.clj
+++ src/demo/core.clj
@@ -48,7 +48,7 @@
 
 (defn haikuify [word-syl-map]
 	(loop [words word-syl-map 
-		   acc '()]
+		   acc []]
 		(if (= (count words) 0)
 			acc
 			(let [haiku (reduce (fn [so-far curr]
@@ -60,12 +60,42 @@
 									(and (< sum-so-far 12) (> new-sum 12)) (reduced [])
 									(and (< sum-so-far 5)  (> new-sum 5))  (reduced [])
 									:else (conj so-far curr))))
-						'({:word "" :syllables 0}) 
+						[{:word "" :syllables 0}] 
 						words)]
 		(recur (rest words) (conj acc haiku))))))
 
+;; first removes each dummy word from the pair
+;; removes empty haikus and those poems at the end of the list that end up smaller than 17 syllables
+(defn groom [haikuified]
+	(let [better (into [] (map (fn [poem] (remove (fn [pair] (= (get pair :syllables) 0)) poem)) haikuified))]
+		(into [] (remove (fn [poem] (or (empty? poem) (< (sum-syllables poem) 17))) better))))
+
+(defn linify [all-haikus]
+	(map (fn [haiku]
+			(into [] 
+				(vals
+					(reduce (fn [lines word]
+								(cond
+									(< (sum-syllables (:first lines))  5) (into lines {:first  (conj (:first lines) word)})
+									(< (sum-syllables (:second lines)) 7) (into lines {:second (conj (:second lines) word)})
+									:else (into lines {:third (conj (:third lines) word)})))
+							{:first [], :second [], :third []}
+							haiku)))) 
+		all-haikus))
+
+(defn just-words [lined]
+	(map (fn [poem] 
+		(map (fn [line] 
+			(str/join " " (map :word line)))
+			poem)) 
+		lined))
+
+(defn make-a-sstring [lined]
+	(str/join "\n\n" (map #(str/join "\n" %) lined)))
+
 (defn -main [& args]
-	(let [words (read-file "text.txt")]
-		(let [haikus (haikuify (map (fn [word] {:word word :syllables (syll-count word)}) words) )]
-			)))
+	(let [words (read-file "bible.txt")]
+		(let [haikus (groom (haikuify (into [] (map (fn [word] {:word word :syllables (syll-count word)}) words))))]
+			(let [lined (linify haikus)]
+				(println (make-a-sstring (just-words lined)))))))
 
